0e6e36b010eaca7552962f6c43c1cae4a245d67a
Adam Palmer <acpalmer1011@gmail.com>
2022-01-11 14:43:47 +0000

    Initial commit

--- BoxingMMATimer/ViewController.swift
+++ BoxingMMATimer/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let trainingTimes = ["Boxing": 180, "MMA": 300]
+    
+    var secondsRemaining = 60
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+
+    @IBAction func TimerSelected(_ sender: UIButton) {
+        
+        let roundChoice = sender.currentTitle! // Boxing, MMA
+        
+        secondsRemaining = trainingTimes[roundChoice]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
+        }
+    }
 
 }
 
